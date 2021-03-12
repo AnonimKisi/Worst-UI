baeaabc153ae16dbce7a3277a0cce97ab48175b6
AnonimKisi <67228486+AnonimKisi@users.noreply.github.com>
2021-03-12 16:12:30 +0300

    Source Code

---
+++ Worst-UI.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Worst_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate name";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "My name is longer";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Length of name: 1 letter";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(77, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Accept name";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 147);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Made by AnonimKisi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Worst-UI.cs
@@ -0,0 +1,221 @@
+﻿//Made by AnonimKisi
+
+using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Worst_UI
+{
+    public partial class Form1 : Form
+    {
+        int length = 1;
+
+        public char RandomName(char letter)
+        {
+            label1.Text = " ";
+            Random random = new Random();
+            int random_number = random.Next(1,27);
+            char[] chars = new char[length];
+            int i = 0;
+            foreach (char letters in chars)
+            {
+                while (i < length)
+                {
+                    i++;                
+                    switch (random_number)
+                    {
+                        case 1:
+                            letter = 'a';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 2:
+                            letter = 'b';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 3:
+                            letter = 'c';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 4:
+                            letter = 'd';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 5:
+                            letter = 'e';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 6:
+                            letter = 'f';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 7:
+                            letter = 'g';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 8:
+                            letter = 'h';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 9:
+                            letter = 'i';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 10:
+                            letter = 'j';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 11:
+                            letter = 'k';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 12:
+                            letter = 'l';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 13:
+                            letter = 'm';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 14:
+                            letter = 'n';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 15:
+                            letter = 'o';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 16:
+                            letter = 'p';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 17:
+                            letter = 'q';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 18:
+                            letter = 'r';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 19:
+                            letter = 's';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 20:
+                            letter = 't';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 21:
+                            letter = 'u';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 22:
+                            letter = 'v';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 23:
+                            letter = 'w';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 24:
+                            letter = 'x';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 25:
+                            letter = 'y';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                        case 26:
+                            letter = 'z';
+                            label1.Text = label1.Text + letter;
+                            Thread.Sleep(20);
+                            random_number = random.Next(1, 9);
+                            break;
+                    }                
+                }
+            }
+            return letter;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && length < 5)
+            {
+                length = length + 1;
+                label2.Text = "Length of name: " + length + " letters";
+            }else if (length == 5)
+            {
+                MessageBox.Show("Can't have a name longer than 5 letters");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RandomName('x');
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your name has been saved as" + label1.Text + ".");
+        }
+    }
+}
