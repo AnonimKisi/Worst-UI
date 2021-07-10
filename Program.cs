289301a166ce6127e0e962bf58df2c1b2379923c
AnonimKisi <67228486+AnonimKisi@users.noreply.github.com>
2021-07-10 19:31:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Worst_UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Worst_UI());
+        }
+    }
+}
